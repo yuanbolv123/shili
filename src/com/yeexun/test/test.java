360a99da6fb02c84a951f12d260afbbff2d69e90
yuanbolv <yuanbo.lv@yeexun.com.cn>
2020-07-23 15:01:09 +0800

    本地工程

---
+++ src/com/yeexun/test/test.java
@@ -0,0 +1,14 @@
+package com.yeexun.test;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("使用git");
+        System.out.println("使用git");
+        System.out.println("使用git");
+        System.out.println("使用git");
+        System.out.println("使用git");
+        System.out.println("使用git");
+
+
+    }
+}
